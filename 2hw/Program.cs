e6a2e72b89513e4c1c2a468b043ba5e018265e37
Bereke Sakan <bereke3103@mail.ru>
2022-11-24 01:23:35 +0600

    is,else

--- 2hw/Program.cs
+++ 2hw/Program.cs
@@ -6,40 +6,16 @@ namespace home2
     {
         static void Main(string[] args)
         {
-            /*double firstValue, secondValue, thirdValue;
+            int a;
+            a = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите ваше первое число:");
-            firstValue = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите ваше второе число");
-            secondValue= double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите ваше третье число");
-            thirdValue= double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Сумма двух чисел: " + firstValue + secondValue);
-
-            Console.Write("Теперь введите ваше третье число");
-            double someRes = double.Parse(Console.ReadLine());
-            someRes = firstValue + secondValue;
-            Console.WriteLine("Сумма трех чисел, поделенный на два: " + (someRes + thirdValue) / 2 );*/
-
-            double firstValue, secondValue, thirdValue;
-
-            Console.WriteLine("Введите первое число");
-            firstValue = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите второе число");
-            secondValue= double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите третье число");
-            thirdValue= double.Parse(Console.ReadLine());
-
-            double plusRes = firstValue + secondValue + thirdValue;
-            double multiRes = firstValue * secondValue * thirdValue;
-
-            Console.WriteLine("Сумма трех чмсел: " + plusRes);
-            Console.WriteLine("Произведение трех числе " + multiRes);
+            if (a >= 5)
+            {
+                Console.WriteLine("Проверка прошла успешна");
+            } else
+            {
+                Console.WriteLine("Ошибка");
+            }
 
 
         }
