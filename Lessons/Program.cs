6f4ee71ca989b18db11defa675aa04350d1c9e05
bereke <bereke3103@mail.ru>
2022-11-30 16:58:29 +0600

    полиморфизм

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -8,51 +8,9 @@ namespace Lessons2
     {
         static void Main(string[] args)
         {
-            //КАЛЬКУЛЯТОР МИНИ
-            /*string str;
-            int a, b;
-
-            Console.WriteLine("Введите ваше первое число");
-
-            str = Console.ReadLine();
-            a = int.Parse(str);
-
-            Console.WriteLine("Введите ваше второе число");
-            str = Console.ReadLine();
-            b = int.Parse(str);
-
-            int result = a + b;
-
-            Console.WriteLine("Ваш ответ: " + result);*/
-
-            //УСЛОВНЫЙ ОПЕРАТОР
-            //string str = "55aa5";
-            //int a;
-            /*bool result = int.TryParse(str, out a);
-            if (result)
-            {
-                Console.WriteLine("Конвертация прошла успешна");
-            } else
-            {
-                Console.WriteLine("Напиши число!!!");
-            }*/
-
-
-            //try__catch
-            /*string str = "555";
-
-            try
-            {
-                int a = int.Parse(str);
-                Console.WriteLine("Конвертация прошла успешна");
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("Напишите верное число");
-            }*/
-
-            //check 
-    }    } 
+            //Полиморфизм
+        }  
+    
+    } 
 }
 
