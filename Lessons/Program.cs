d0f6708b0cdc62c880a1b43deeadf22183dd3a00
bereke <bereke3103@mail.ru>
2022-12-02 11:21:20 +0600

    конструктор

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -4,46 +4,9 @@
 
 namespace Lessons2
 {
-    class Program
+    class Gun
     {
-        //static void Main(string[] args)
-        //{
-        //    while (true)
-        //    {
-        //        ConsoleKey key = Console.ReadKey().Key;
-
-        //        int keyCode = (int)key;
-
-        //        Console.WriteLine($"\tEnum {key}\tKey code {keyCode}");
-
-        //        if (key == ConsoleKey.Enter)
-        //        {
-        //            Console.WriteLine("Вы нажали enter!");
-        //        }
-        //    }
-        //}
-
-
-        enum DayOfWeek
-        {
-            Monday = 5,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
-        }
-
-        static void Main(string[] args)
-        {
-            DayOfWeek firstDay = DayOfWeek.Monday;
-            DayOfWeek secondDay = DayOfWeek.Tuesday;
-
-            Console.WriteLine(Enum.GetUnderlyingType(typeof(DayOfWeek)));
-            Console.WriteLine((int)secondDay);
-        }
-
+       //конструктор
     }
 }
 
