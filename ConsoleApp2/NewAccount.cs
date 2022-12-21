8d91e6ddf02c4bbb4477074a3dd14a19833e9e36
Bereke Sakan <bereke3103@mail.ru>
2022-12-22 00:41:50 +0600

    delegate

---
+++ ConsoleApp2/NewAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class NewAccount
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(100);
+            account.RegisterHandler(Display);
+            account.RegisterHandler(ColorDisplay);
+            account.Put(100);
+            account.Withdraw(100);
+            account.UnRegisterHandler(Display);
+            //account.UnRegisterHandler(ColorDisplay);
+            account.Withdraw(600);
+
+       
+
+            Console.ReadKey();
+
+            
+        }
+        static void Display(string message)
+        {
+            Console.WriteLine(message);
+        }
+        
+        static void ColorDisplay(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(message);
+        
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    //определяем делегат
+    delegate void AccountStateHandler(string message);
+    class Account
+    {
+        //хранит средства на счете
+        int _sum;
+        //определим в этом классе переменную делегата
+        AccountStateHandler _del; //равна значению null
+        //чтоб этот делегат установить, определяем метод
+        public void RegisterHandler(AccountStateHandler del)
+        {
+            _del += del;
+        }
+        
+        public void UnRegisterHandler(AccountStateHandler del)
+        {
+            _del -= del;
+        }
+        public Account(int sum)
+        {
+            //с помощью конструктора передаем
+            //этой переменной некоторые значения
+            _sum = sum;
+        }
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+                if(_del != null)
+                    _del($"На счёт пришло {sum}");
+
+        }
+        public void Withdraw(int sum)
+        {
+            if (_sum >= sum)
+            {
+                _sum -= sum;
+                if(_del != null)
+                    _del($"Со счёта ушло {sum}");
+            } else
+            {
+                if (_del != null)
+                    _del("Недостаточно средств на балансе");
+            }
+        }
+    }
+
+    //========================================================================
+    //
+}
