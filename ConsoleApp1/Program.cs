010bd01ebc142a444bb1b35b51224d3b2d3c52de
Bereke Sakan <bereke3103@mail.ru>
2023-01-08 01:07:16 +0600

    ref-out

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //void RefNumber(ref int num)
+            //{
+            //    num++;
+            //    Console.WriteLine($"ЦИФРА ШЕСТЬ: {num}");
+            //}
+
+            //int number = 5;
+            //Console.WriteLine($"Здесь будет пять: {number}");
+
+            //PrintNumber(ref number);
+
+            //Console.WriteLine($"ЗДЕСЬ БУДЕТ ШЕСТЬ: {number}");
+
+            //Console.ReadLine();
+
+
+            
+     //=======================================================
+     
+     
+     
+            //void OutNumber(int a, int b, out int result)
+            //{
+            //    result = a + b;
+            //}
+
+            //int number;
+
+            //int oneA = 5;
+
+            //int oneB = 5;
+
+            //OutNumber(oneA, oneB, out number);
+
+            //Console.WriteLine($"{number}");
+
+            //Console.ReadLine();
+
+        }
+    }
+}
