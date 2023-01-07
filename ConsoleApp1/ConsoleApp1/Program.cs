ae18edab70822472e9e3aa11a9d7a3391e6b1fee
bereke <bereke3103@mail.ru>
2023-01-07 18:53:20 +0600

    console

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bereke";
+            int age = 23;
+            double height = 1.81;
+            Console.WriteLine("Имя: {0}, Возраст: {1}, Рост: {2}", name, age, height);
+            Console.ReadLine();     
+        }
+    }
+}
