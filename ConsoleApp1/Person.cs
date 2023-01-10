c6707c90c3a9377b333078b2ad096bc251bbbee8
bereke <bereke3103@mail.ru>
2023-01-10 10:15:19 +0600

    check

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        private string _name = "";
+
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public void CheckName()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person() { Name = "Bereke" };
+
+            person.CheckName();
+
+            //============================================================
+
+            //так как уже был унаследован от Person мы можем переиспользовать его
+
+            Employee employee = new Employee() { Name = "Employee" };
+
+            employee.CheckName();
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
