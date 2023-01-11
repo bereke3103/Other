fe5874719fc96d37d123a199979a5280460e445c
bereke <bereke3103@mail.ru>
2023-01-11 09:34:33 +0600

    check

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+      public object Id { get; }
+        public string Name { get; }
+        public Person(object id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(123, "Bereke");
+            Person person1 = new Person("123", "Bereke");
+
+            int berekeInt = (int)person.Id;
+            string berekeString = (string)person1.Id;
+
+            Console.WriteLine(berekeInt);
+            Console.WriteLine(berekeString);
+            Console.ReadLine();
+        }
+    }
+}
