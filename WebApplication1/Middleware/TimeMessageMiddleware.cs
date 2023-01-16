33e149327846f7cb427a3de5d2a56cf16eed68d4
bereke <bereke3103@mail.ru>
2023-01-16 15:29:06 +0600

    smm

---
+++ WebApplication1/Middleware/TimeMessageMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using WebApplication1.Service;
+
+namespace WebApplication1.Middleware
+{
+    public class TimeMessageMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public TimeMessageMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, ITimeService service)
+        {
+            context.Response.ContentType = "text/html; charset=utf-8";
+            await context.Response.WriteAsync($"<h1>Time: {service.GetTtime()}</h1>");
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,19 +1,22 @@
 using System.Text;
+using WebApplication1.Middleware;
 using WebApplication1.Service;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder();
 
 //IServiceCollection services = builder.Services;
-builder.Services.AddTimeServices();
+builder.Services.AddTransient<ITimeService, ShortTimeService>();
 
 WebApplication app = builder.Build();
 
-app.Run(async context =>
-{
-    var timeService = app.Services.GetService<TimeService>();
-    context.Response.ContentType = "text/html; charset=utf-8";
-    await context.Response.WriteAsync($"Time: {timeService?.GetTime()}");
-});
+app.UseMiddleware<TimeMessageMiddleware>();
+
+//app.Run(async context =>
+//{
+//    var timeService = app.Services.GetService<TimeService>();
+//    context.Response.ContentType = "text/html; charset=utf-8";
+//    await context.Response.WriteAsync($"Time: {timeService?.GetTime()}");
+//});
 
 
 
