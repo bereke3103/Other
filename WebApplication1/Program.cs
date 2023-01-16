bc81f974263c873194022a94e333db109019e099
bereke <bereke3103@mail.ru>
2023-01-16 13:42:16 +0600

    middle

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,25 +1,19 @@
+using WebApplication1;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder();
 
 WebApplication app = builder.Build();
 
-app.Map("/home", appBuilder =>
-{
-    appBuilder.Map("/index", Index);
-    appBuilder.Map("/about", About);
 
-    appBuilder.Run(async (context) => await context.Response.WriteAsync("Home page"));
+//app.UseMiddleware<TokenMiddleware>();
+
+app.UseToken();
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Hello metanit .com");
 });
 
-app.Run(async (context) => await context.Response.WriteAsync("Page not found"));
+
 
 app.Run();
-
-void Index(IApplicationBuilder build)
-{
-    build.Run(async context => await context.Response.WriteAsync("Index page"));
-}
-
-void About (IApplicationBuilder build)
-{
-    build.Run(async context => await context.Response.WriteAsync($"{nameof(About)}"));
-}
---
+++ WebApplication1/TokenExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    
+    public static class TokenExtension
+    {
+        public static IApplicationBuilder UseToken(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<TokenMiddleware>();
+        }
+    }
+}
