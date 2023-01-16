ee770d7d714323244984a75a0b7e2ebfdfd2abcb
bereke <bereke3103@mail.ru>
2023-01-16 13:26:47 +0600

    mapping

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,29 +2,24 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder();
 
 WebApplication app = builder.Build();
 
-
-
-app.UseWhen(context => context.Request.Path == "/time",
-    appBuilder =>
-    {
-        appBuilder.Use(async (context, next) =>
-        {
-            var time = DateTime.Now.ToShortDateString();
-            Console.WriteLine($"Time: {time}");
-            await next();
-        });
-
-        appBuilder.Run(async context =>
-        {
-            var time = DateTime.Now.ToShortTimeString();
-            await context.Response.WriteAsync($"Time: {time}");
-        });
-
-    });
-
-app.Run(async context =>
+app.Map("/home", appBuilder =>
 {
-    await context.Response.WriteAsync("HELLO METANIT COM");
+    appBuilder.Map("/index", Index);
+    appBuilder.Map("/about", About);
+
+    appBuilder.Run(async (context) => await context.Response.WriteAsync("Home page"));
 });
 
+app.Run(async (context) => await context.Response.WriteAsync("Page not found"));
+
 app.Run();
+
+void Index(IApplicationBuilder build)
+{
+    build.Run(async context => await context.Response.WriteAsync("Index page"));
+}
+
+void About (IApplicationBuilder build)
+{
+    build.Run(async context => await context.Response.WriteAsync($"{nameof(About)}"));
+}
